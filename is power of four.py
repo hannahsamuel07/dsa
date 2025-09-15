1d5ba2869f099fce50bbcee7639f4fe9b1dfefd5
hannahsamuel07 <hannahsamuel@ucla.edu>
2025-09-15 11:57:42 -0700

    is power of four

---
+++ is power of four.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def isPowerOfFour(self, n):
+        """
+        :type n: int
+        :rtype: bool
+        """
+        if n < 0:
+            return False
+        if n == 1:
+            return True
+        if n == 0:
+            return False
+        while n > 0:
+            if n == 4: return True
+            if n%4 != 0:
+                return False
+            n//=4
+        return True
