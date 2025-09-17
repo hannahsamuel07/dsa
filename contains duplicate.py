80bad12fc99da726672b6f902d71978139129241
hannahsamuel07 <hannahsamuel@ucla.edu>
2025-09-17 15:38:10 -0700

    power of two

---
+++ contains duplicate.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        nums.sort()
+        for i in range(0,len(nums)):
+            if i != len(nums) - 1 and nums[i] == nums[i+1]:
+                return True
+
+        return False
